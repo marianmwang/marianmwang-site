c1ec3c11256aa3afad99cdc39736c301c74aed72
marianmwang <marian.mian.wang@gmail.com>
2021-11-04 17:08:45 -0400

    adding icons

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link, animateScroll as scroll } from "react-scroll";
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 import "../pages/styles.css";
 
 const Layout = ({ children }) => {
   const scrollToTop = () => {
-    scroll.scrollToTop({duration: 1000});
+    scroll.scrollToTop({ duration: 1000 });
   };
 
   return (
     <div className="centered">
-        <div className="sidenav">
-          <div className="sidenav-section">
+      <div className="sidenav">
+        <div className="sidenav-section">
           <h1>
             <button className="button-no-style" onClick={scrollToTop}>
               marian<br></br>wang
             </button>
           </h1>
-          </div>
-          <div className="sidenav-section">
+        </div>
+        <div className="sidenav-section">
           <ul>
             <li>
               <Link
@@ -61,29 +62,33 @@ const Layout = ({ children }) => {
               </Link>
             </li>
           </ul>
-          </div>
-          <div className="sidenav-section">
+        </div>
+        <div className="sidenav-section">
           <ul>
-          <li><a href="https://drive.google.com/file/d/1RyUoUS1dxxd8SvV8xkZ0FKMLNhwFXgW4/view?usp=sharing">
-            <FontAwesomeIcon icon="fa-solid fa-envelope" />resume
-          </a>
-          </li>
-          <li>
-          <a href="mailto:marian.mian.wang@gmail.com">
-            <FontAwesomeIcon icon="fa-solid fa-envelope" />email
-            <i class="fa-solid fa-user"></i>
-          </a>
-          </li>
-          <li>
-          <a href="https://github.com/marianmwang">
-            <FontAwesomeIcon icon="fa-brands fa-github" />github
-          </a>
-          </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1RyUoUS1dxxd8SvV8xkZ0FKMLNhwFXgW4/view?usp=sharing">
+                <FontAwesomeIcon icon="fas envelope" />
+                resume
+              </a>
+            </li>
+            <li>
+              <a href="mailto:marian.mian.wang@gmail.com">
+                <FontAwesomeIcon icon="fas fa-envelope" />
+                <FontAwesomeIcon icon={"faEnvelope"} />
+                email
+                <i class="fas fa-paper-plane"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/marianmwang">
+                <FontAwesomeIcon icon="fa-brands fa-github" />
+                github
+              </a>
+            </li>
           </ul>
-          </div>
         </div>
-        <div className="content">{children}
-        </div>
+      </div>
+      <div className="content">{children}</div>
     </div>
   );
 };

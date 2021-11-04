a1c5708e6cd5aea8ecd7d3491452c4d3415625d9
marianmwang <marian.mian.wang@gmail.com>
2021-11-04 16:44:08 -0400

    Updates

---
+++ precache-manifest.569c4ef9b5423504fd10fa2d9570a43b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d735751ba11489dd856b25711c8fede5",
+    "url": "/marianmwang-site/index.html"
+  },
+  {
+    "revision": "4ec0be19a18f2ca41645",
+    "url": "/marianmwang-site/static/css/main.e2e67417.chunk.css"
+  },
+  {
+    "revision": "46adc08ab18c94091560",
+    "url": "/marianmwang-site/static/js/2.6c4a79dc.chunk.js"
+  },
+  {
+    "revision": "12d98599ee75686ece6379c641ecd705",
+    "url": "/marianmwang-site/static/js/2.6c4a79dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e16844e92c37981dce5",
+    "url": "/marianmwang-site/static/js/3.8f3cd084.chunk.js"
+  },
+  {
+    "revision": "4ec0be19a18f2ca41645",
+    "url": "/marianmwang-site/static/js/main.c252b137.chunk.js"
+  },
+  {
+    "revision": "73530f7c4c7249e1a0ee",
+    "url": "/marianmwang-site/static/js/runtime-main.efcc3eb5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/marianmwang-site/static/media/logo.06e73328.svg"
+  }
+]);

e5cd72f950658a2515b75820babf46cc6409297b
marianmwang <marian.mian.wang@gmail.com>
2021-11-04 00:49:02 -0400

    centered content

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -5,39 +5,43 @@ import "../pages/styles.css";
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <div className="sidenav">
-        <h1>
-          marian<br></br>wang
-        </h1>
-        <ul>
-          <li>
-            <a className="sidenav-link" href="/">
-              about me
-            </a>
-          </li>
-          <li>
-            <a className="sidenav-link" href="#projects">
-              projects
-            </a>
-          </li>
-          <li>
-            <a className="sidenav-link" href="#gallery">
-              gallery
-            </a>
-          </li>
-        </ul>
-        <a href="https://drive.google.com/file/d/1RyUoUS1dxxd8SvV8xkZ0FKMLNhwFXgW4/view?usp=sharing">
-          <FontAwesomeIcon icon="fa-solid fa-envelope" />resume |
-        </a>
-        <a href="mailto:marian.mian.wang@gmail.com">
-          <FontAwesomeIcon icon="fa-solid fa-envelope" /> email |<i class="fa-solid fa-user"></i>
-        </a>
-        <a href="https://github.com/marianmwang">
-          <FontAwesomeIcon icon="fa-brands fa-github" /> github
-        </a>
+    <div className="centered">
+      <div>
+        <div className="sidenav">
+          <h1>
+            marian<br></br>wang
+          </h1>
+          <ul>
+            <li>
+              <a className="sidenav-link" href="/">
+                about me
+              </a>
+            </li>
+            <li>
+              <a className="sidenav-link" href="#projects">
+                projects
+              </a>
+            </li>
+            <li>
+              <a className="sidenav-link" href="#gallery">
+                gallery
+              </a>
+            </li>
+          </ul>
+          <a href="https://drive.google.com/file/d/1RyUoUS1dxxd8SvV8xkZ0FKMLNhwFXgW4/view?usp=sharing">
+            <FontAwesomeIcon icon="fa-solid fa-envelope" />
+            resume |
+          </a>
+          <a href="mailto:marian.mian.wang@gmail.com">
+            <FontAwesomeIcon icon="fa-solid fa-envelope" /> email |
+            <i class="fa-solid fa-user"></i>
+          </a>
+          <a href="https://github.com/marianmwang">
+            <FontAwesomeIcon icon="fa-brands fa-github" /> github
+          </a>
+        </div>
+        <div className="content">{children}</div>
       </div>
-      <div className="content">{children}</div>
     </div>
   );
 };

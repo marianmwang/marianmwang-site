c2a7cd24e968c5f81fff5876c9052235e681eb82
Marian Wang <marian.mian.wang@gmail.com>
2022-06-26 21:15:39 -0400

    starting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import './App.css';
-import React from 'react';
-import Projects from './pages/projects.jsx';
-import Layout from './pages/layout.jsx';
-import About from './pages/about.jsx';
-import Gallery from './pages/gallery.jsx';
-import Type from './pages/typewriter.jsx'
-// import AboutImages from './pages/aboutImages.jsx';
+import React from "react";
+import GenericScreen from "./screens/GenericScreen";
+import './app.css'
 
 function App() {
-  return (
-    <Layout>
-      {/* <AboutImages /> */}
-      <Type />
-      <About />
-      <Projects />
-      <Gallery />
-    </Layout>
-  );
+  return <GenericScreen />;
 }
 
 export default App;

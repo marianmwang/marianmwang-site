22314e397dc98f6c79536314f10619152dc43771
Marian Wang <marian.mian.wang@gmail.com>
2022-12-28 20:42:32 -0800

    clean up old files and add mui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
 import React from 'react';
-import Projects from './pages/projects.jsx';
-import Layout from './pages/layout.jsx';
-import About from './pages/about.jsx';
-import Gallery from './pages/gallery.jsx';
-import Type from './pages/typewriter.jsx'
-// import AboutImages from './pages/aboutImages.jsx';
+
 
 function App() {
   return (

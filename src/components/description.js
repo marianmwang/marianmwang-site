b2891fe3105c9621fb5ea34d4251233187d6e83f
Marian Wang <marian.mian.wang@gmail.com>
2023-05-10 16:11:09 -0700

    Update description for may 2023

--- src/components/description.js
+++ src/components/description.js
@@ -33,29 +33,19 @@ function Description() {
     <>
       <StyledText>Hey there! I'm Marian.</StyledText>
       <StyledText>
-        I'm currently interning at{" "}
-        <StyledLink href="https://www.brilliant.tech/">
-          Brilliant.tech
-        </StyledLink>{" "}
-        in San Mateo, CA. Previously, I interned at{" "}
-        <StyledLink href="https://www.twitter.com/">Twitter</StyledLink> and{" "}
-        <StyledLink href="https://www.amazon.com/">Amazon</StyledLink>, and I'm
-        headed to{" "}
-        <StyledLink href="https://www.khanacademy.org/">
-          Khan Academy
-        </StyledLink>{" "}
-        this summer.
+        I'm currently on a break, getting ready to start my internship at{" "}
+        <StyledLink href="https://www.brilliant.tech/">Khan Academy</StyledLink>{" "}
+        in Mountain View, CA this summer. Previously, I interned at{" "}
+        <StyledLink href="https://www.twitter.com/">Twitter</StyledLink>,{" "}
+        <StyledLink href="https://www.amazon.com/">Amazon</StyledLink>, and{" "}
+        <StyledLink href="https://www.amazon.com/">Brilliant.tech</StyledLink>.
       </StyledText>
       <StyledText>
         In my free time, you can find me enjoying coffee and a{" "}
         <StyledLink href="https://marianmwang.notion.site/media-list-bb1a75b309b3413bbce4d038be6c77f8">
           good book
         </StyledLink>
-        ,{" "}
-        <StyledLink href="https://www.instagram.com/flarethey/">
-          drawing fanart
-        </StyledLink>
-        , or climbing rocks at my local bouldering gym.
+        , climbing at my local bouldering gym, or drawing digital art.
       </StyledText>
       <StyledText>
         Let's chat!{" "}
